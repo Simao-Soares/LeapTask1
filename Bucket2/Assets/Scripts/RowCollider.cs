46135565a3ec1ce81be8c86af45ec56a8b75ac9c
Simao-Soares <simao.pedrosoares@gmail.com>
2021-10-25 19:03:28 +0100

    row collider not working

--- Bucket2/Assets/Scripts/RowCollider.cs
+++ Bucket2/Assets/Scripts/RowCollider.cs
@@ -11,18 +11,16 @@ public class RowCollider : MonoBehaviour
     public EventTrigger.TriggerEvent RowRight;
 	private static GameObject gameManager;
 
-    public static GameObject UICanvas;
-
     private void OnCollisionEnter(Collision collisionInfo) {
-
-		if (collisionInfo.collider.name == "ExtensionL")
+		
+		if (collisionInfo.collider.tag == "Green")
         {
             Debug.Log("Left Row!");
             BaseEventData eventData = new BaseEventData(EventSystem.current);
             this.RowLeft.Invoke(eventData);
         }
 
-        else if (collisionInfo.collider.name == "ExtensionR")
+		else if (collisionInfo.collider.tag == "Green")
         {
              Debug.Log("Right Row!");
             BaseEventData eventData = new BaseEventData(EventSystem.current);
